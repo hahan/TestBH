d45e1f52afb639de97e6d8085d35cd912b9e7ee8
hahan <hakim.hanif@gmail.com>
2021-11-05 18:14:30 -0700

    removing code to fix the issue

--- EventHandler.java
+++ EventHandler.java
@@ -94,7 +94,7 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
         final AmazonS3 s3Client = EventHandler.getS3Client();
         logger.log("Processing Bucket: " + bucketName);
 
-        ObjectListing files = s3Client.listObjects(bucketName);
+        
         List<KeyVersion> filesProcessed = new ArrayList<DeleteObjectsRequest.KeyVersion>();
 
         for (Iterator<?> iterator = files.getObjectSummaries().iterator(); iterator.hasNext(); ) {
